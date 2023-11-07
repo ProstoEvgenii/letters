bbb82a5e5a4dab0d02beffec89e1fd2f17b1fd47
ProstoEvgenii <98259817+ProstoEvgenii@users.noreply.github.com>
2023-11-07 11:43:41 +0300

    .

--- main.go
+++ main.go
@@ -3,17 +3,14 @@ package main
 import (
 	"letters/db"
 	"letters/functions"
-	"log"
-
-	"github.com/joho/godotenv"
 )
 
 func main() {
-	if err := godotenv.Load(".env"); err != nil {
-		log.Println("Файл .env не найден")
-		// time.Sleep(10 * time.Second)
-		// log.Fatal()
-	}
+	// if err := godotenv.Load(".env"); err != nil {
+	// 	log.Println("Файл .env не найден")
+	// time.Sleep(10 * time.Second)
+	// log.Fatal()
+	// }
 	db.Connect()
 	functions.AutoSend()
 	// getStatusToday()
